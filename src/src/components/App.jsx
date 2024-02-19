53aeb702d5702350a002ae33a93796289cd4f854
mini109 <derdriumcateer@gmail.com>
2024-02-19 16:27:15 +1000

    footer added as login / singup pages

--- src/src/components/App.jsx
+++ src/src/components/App.jsx
@@ -1,6 +1,11 @@
+import '../styling/app.css'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './Home'
 import NavigationBar from './NavBar'
+import Footer from './Footer'
+import '../styling/app.css'
+import Login from './Login'
+import Register from './Register'
 
 function App() {
 
@@ -9,7 +14,10 @@ function App() {
       <NavigationBar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
       </Routes>
+      <Footer/>
     </Router>
   )
 }
--- src/src/components/NavBar.jsx
+++ src/src/components/NavBar.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown, Button, Container } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Button, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 const NavigationBar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg">
       <Container>
-        <Navbar.Brand href="/">Logo</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Logo</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mx-auto">
-            <Nav.Link href="#home">Buses</Nav.Link>
-            <Nav.Link href="#link">Plan Your Trip</Nav.Link>
-            <Nav.Link href="#link">My Trips</Nav.Link>
+            <Nav.Link as={Link} to="/">Buses</Nav.Link>
+            <Nav.Link as={Link} to="/">Plan Your Trip</Nav.Link>
+            <Nav.Link as={Link} to="/">My Trips</Nav.Link>
             <NavDropdown title="Admin" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Routes</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Users</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Reservations</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Locations</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/">Routes</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/">Users</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/">Reservations</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/">Locations</NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Nav className="ml-auto">
-            <Nav.Link href="#link"><Button variant="outline-primary">Sign Up</Button></Nav.Link>
-            <Nav.Link href="#link"><Button variant="outline-info">Login</Button></Nav.Link>
+          <Nav.Link as={Link} to="/register"><Button variant="outline-primary">Sign Up</Button></Nav.Link>
+          <Nav.Link as={Link} to="/login"><Button variant="outline-info">Login</Button></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
