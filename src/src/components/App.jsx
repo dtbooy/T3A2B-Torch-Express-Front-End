7e56bd1f5e0fe476e9bc84358742352e248666f1
mini109 <derdriumcateer@gmail.com>
2024-02-19 12:45:15 +1000

    basic navigation bar

--- src/src/components/App.jsx
+++ src/src/components/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './Home'
+import NavigationBar from './NavBar'
 
 function App() {
 
   return (
     <Router>
+      <NavigationBar/>
       <Routes>
         <Route path="/" element={<Home/>}/>
       </Routes>
