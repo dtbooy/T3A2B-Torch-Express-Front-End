6aa63b1e211e23956e38a2151dd53505cde7ed94
dtbooy <dtbooy@gmail.com>
2024-02-23 14:33:17 +1000

    revert Serach results

--- src/src/components/Search Page/SearchResults.jsx
+++ src/src/components/Search Page/SearchResults.jsx
@@ -1,21 +1,44 @@
-import { Button, Card, Col, Row, Spinner } from "react-bootstrap" 
-import { Link } from "react-router-dom" 
+import { Button, Card, Col, Row, Spinner } from "react-bootstrap";
+import BookTicket from "./BookTicket";
+import { useState } from "react";
 
 const SearchResults = (params) => {
-  let { results, locations } = params 
+  let { results, locations } = params;
+
+  // holds the service selected for booking
+  const [selectedService, setSelectedService] = useState({});
+  // controls the booking Modal visibility
+  const [showBooking, setShowBooking] = useState(false);
+
+  // load the service details into selectedService state & show booking modal
+  const handleClick = (e) => {
+    let service = results.find((res) => res._id == e.target.id);
+    service = {
+      ...service,
+      dropoffLocation: locations.find(
+        (loc) => loc._id === service.dropoffLocation
+      ).name,
+      pickupLocation: locations.find(
+        (loc) => loc._id === service.pickupLocation
+      ).name,
+    };
+    setSelectedService(service);
+    // Show the BookTicket Modal
+    setShowBooking(true);
+  };
 
   // move this into a utility functions file
   // Calculate arrival time
   function addTime(time, timeOffset) {
-    let newTime = new Date(time) 
-    newTime.setMinutes(newTime.getMinutes() + parseInt(timeOffset)) 
-    return newTime 
+    let newTime = new Date(time);
+    newTime.setMinutes(newTime.getMinutes() + parseInt(timeOffset));
+    return newTime;
   }
 
   if (results && results.length === 0) {
-    return <h4> No Matches found... </h4> 
+    return <h4> No Matches found... </h4>;
   } else if (!results) {
-    return <></> 
+    return <></>;
   } else if (results == "loading") {
     return (
       <>
@@ -27,47 +50,66 @@ const SearchResults = (params) => {
     );
   } else {
     return (
-      <Row>
-        {results.map((result) => {
-          return (
-            <Col key={result._id} sm={12} md={6} xl={4}>
-              <Card>
-                <Card.Body>
-                  <Card.Title>{result.eventName}</Card.Title>
-                  <Card.Text>Date: {(new Date(result.collectionTime)).toDateString()}</Card.Text>
-                  <Card.Text>
-                    Departing:{" "}
-                    {
-                      locations.find((loc) => loc._id === result.pickupLocation)
-                        .name
-                    }{" "}
-                    at {result.collectionTime.slice(11, 16)}
-                  </Card.Text>
-                  <Card.Text>
-                    Arriving:{" "}
-                    {
-                      locations.find(
-                        (loc) => loc._id === result.dropoffLocation
-                      ).name
-                    }{" "}
-                    at{" "}
-                    {addTime(result.collectionTime, result.estimatedTravelTime)
-                      .toISOString()
-                      .slice(11, 16)}
-                    (est)
-                  </Card.Text>
-                  <Card.Text>Tickets remaining: {result.capacity - result.reservations}</Card.Text>
-                  <Link to={`/Book:${result._id}`}>
-                  <Button variant="primary">Book a seat</Button>
-                  </Link>
-                </Card.Body>
-              </Card>
-            </Col>
-          ) 
-        })}
-      </Row>
-    ) 
+      <>
+        <Row className="row-gap-4">
+          {results.map((result) => {
+            return (
+              <Col key={result._id} sm={12} md={6} xl={4}>
+                <Card>
+                  <Card.Body className="flex-column align-items-start ">
+                    <Card.Title>{result.eventName}</Card.Title>
+                    <Card.Text>
+                      Date: {new Date(result.collectionTime).toDateString()}
+                    </Card.Text>
+                    <Card.Text>
+                      Departing:{" "}
+                      {
+                        locations.find(
+                          (loc) => loc._id === result.pickupLocation
+                        ).name
+                      }{" "}
+                      at {result.collectionTime.slice(11, 16)}
+                    </Card.Text>
+                    <Card.Text>
+                      Arriving:{" "}
+                      {
+                        locations.find(
+                          (loc) => loc._id === result.dropoffLocation
+                        ).name
+                      }{" "}
+                      at{" "}
+                      {addTime(
+                        result.collectionTime,
+                        result.estimatedTravelTime
+                      )
+                        .toISOString()
+                        .slice(11, 16)}
+                      (est)
+                    </Card.Text>
+                    <Card.Text>
+                      Tickets remaining: {result.capacity - result.reservations}
+                    </Card.Text>
+                    <Button
+                      id={result._id}
+                      onClick={(e) => handleClick(e)}
+                      variant="primary"
+                    >
+                      Book a seat
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+          <BookTicket
+            showBooking={showBooking}
+            setShowBooking={setShowBooking}
+            selectedService={selectedService}
+          />
+        </Row>
+      </>
+    );
   }
-} 
+};
 
-export default SearchResults 
+export default SearchResults;
