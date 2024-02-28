cfaa9a6daac93d62196cbfb32af05e6e7ebb0d41
maxhandley97 <maxhandley97@gmail.com>
2024-02-28 17:14:00 +1100

    basic testing features for login and  mytrips

--- src/src/main.jsx
+++ src/src/main.jsx
@@ -4,7 +4,9 @@ import './index.css'
 import App from './components/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-        <App />
-  </React.StrictMode>,
+  <>
+   <App />
+  </>
+       
+  
 )
