4dbcda8187842907d20f1fd7ea44b98f6ae704cf
maxhandley97 <maxhandley97@gmail.com>
2024-02-29 16:26:05 +1100

    added functional register test

--- src/src/tests/Login.test.jsx
+++ src/src/tests/Login.test.jsx
@@ -1,16 +1,14 @@
 
 import '@testing-library/jest-dom'
 import { screen, render, waitFor, fireEvent } from '@testing-library/react'
-import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
 import App from './../components/App'
 import NavigationBar from '../components/NavBar'
 import { BrowserRouter } from 'react-router-dom'
-import Login from '../components/Login'
 import userEvent from '@testing-library/user-event'
-import Cookies from 'js-cookie'
 import renderWithRouter from './SetupTests/TestUtils'
 
-let token
+let container
 
 describe('app rendering and navigation', () => {
   let container
@@ -19,29 +17,19 @@ describe('app rendering and navigation', () => {
     render(<App />).container
   })
 
-  it('renders homepage successfully', () => {
-    expect(screen.getByText('Get On Board!')).toBeInTheDocument()
-  })
-
   it('renders login button in navbar and is clickable', async () => {
     const loginButton = screen.getByRole('button', { name: "Login" })
     userEvent.click(loginButton)
     await waitFor(() => {
-      expect(container.querySelector('h1')).toHaveTextContent("Login")
+      expect(container.querySelector('h1')).toHaveTextContent('Login')
     })
 
     
-  })
-  it('Login page is rendered after clicked', async () => {
-    const signUpHeader = await screen.findByText('Sign Up')
-    expect(signUpHeader).toBeInTheDocument()
   })
 })
 
 
 describe('Login Component', () => {
-  let container
-
   beforeEach(() => {
     container = renderWithRouter(
       <App/>,
@@ -53,25 +41,16 @@ describe('Login Component', () => {
     expect(container.querySelector('h1')).toHaveTextContent("Login")
   })
 
-
-  it('tests logging in', async () => {
-   
-    const emailInput = screen.getByLabelText('Email')
-    const passwordInput = screen.getByLabelText('Password')
-    const submitButton = screen.getAllByRole('button', { name: 'Login' })[1]
-
-    
-    userEvent.type(emailInput, 'admin@example.com')
-    userEvent.type(passwordInput, 'admin1234')
-
-    userEvent.click(submitButton)
-
-      await waitFor(() => {
-        expect(Cookies.get('accessToken')).toBeTruthy()
+  it('successful login redirects to home page', async () => {
  
-
-    })
+  fireEvent.change(screen.getByLabelText('Email'), { target: { value: 'admin@example.com' } });
+  fireEvent.change(screen.getByLabelText('Password'), { target: { value: 'admin1234' } });
+  fireEvent.click(screen.getAllByRole('button', { name: 'Login' })[1])
+  
+  await waitFor(() => {
+    expect(window.location.pathname).toBe('/')
   })
+})
 })
 
 //  Assuming `testToken` is declared outside the test scope
